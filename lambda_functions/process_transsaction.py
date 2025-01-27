4cdcfea61d5f035da8b0ad1f3c549ce83ac60ea9
Andrehlb <andreh.72@hotmail.com>
2025-01-27 07:16:58 -0300

    feat(lambda): create `process_transcription.py` script

---
+++ lambda_functions/process_transsaction.py
@@ -0,0 +1,19 @@
+import json
+def lambda_handler(event, context):
+    #aqui simula processamento de transação
+    transaction = event("transaction", {})
+    user_id = transaction.get("user_id", "unknown")
+    amount = transaction.get("amount", 0)
+
+    #aqui vou simular uma resposta de sucesso da transação
+    response = {
+        "status": "success",
+        "user_id": user_id,
+        "processed_amount": amount,
+        "message": "A transação foi feita co sucesso"
+    }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(response)
+    }
